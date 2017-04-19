0d2009a782506daffcd92c17c8dc438d7001c007
Daniel Sumara <daniel.sumara@gmail.com>
2017-04-19 19:55:53 +0200

    Create framework

---
+++ CoreDataStack/CoreDataStack.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataStack.h
+//  CoreDataStack
+//
+//  Created by Daniel Sumara on 19.04.2017.
+//  Copyright © 2017 Daniel Sumara. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreDataStack.
+FOUNDATION_EXPORT double CoreDataStackVersionNumber;
+
+//! Project version string for CoreDataStack.
+FOUNDATION_EXPORT const unsigned char CoreDataStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataStack/PublicHeader.h>
+
+
